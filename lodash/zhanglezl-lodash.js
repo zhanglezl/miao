002e34c1fbfe8c721af652410db503ec79e90759
zhanglezl <652366290@qq.com>
2020-12-29 14:30:38 +0800

    asdf

--- lodash/zhanglezl-lodash.js
+++ lodash/zhanglezl-lodash.js
@@ -53,12 +53,14 @@ var zhanglezl = function () {
     }
 
     function findIndex(ary, test, from = 0) {
+        var test = process(test) 
         for (var i = from; i < ary.length; i++) {
             if (test(ary[i])) return i
         }
         return -1    
     }
     function findLastIndex(ary, test, from = ary.length - 1) {
+        var test = process(test) 
         for (var i = from; i >= 0; i--) {
             if (test(ary[i])) return i
         }
@@ -82,7 +84,7 @@ var zhanglezl = function () {
         var i = 0
         while(i < ary.length) {
             if (Array.isArray(ary[i])) {
-                res.push(...flattendeep(ary[i])) // res = res.concat(flattendeep(ary[i]))
+                res.push(...flattenDeep(ary[i])) // res = res.concat(flattendeep(ary[i]))
                 i++
             } else { 
                 res.push(ary[i++])
@@ -99,7 +101,7 @@ var zhanglezl = function () {
                   res.push(ary[i])    
                 } else {
                     depth--
-                    res = res.concat(flattendepth(ary[i],depth))
+                    res = res.concat(flattenDepth(ary[i],depth))
                 }
             } else {
                 res.push(ary[i])
@@ -109,13 +111,8 @@ var zhanglezl = function () {
     }
     
     function fromPairs(pairs) {
-        var res = {}
-        if (pairs.length == 2) {
-            res[pairs[0]] = pairs[1]
-            return res
-        }
-        for (var i = 0,j = 0; i < pairs.length; i++) {
-            res[pairs[i][j]] = pairs[i][j + 1]
+        for (var i = 0; i < pairs.length; i++) {
+            res[pairs[i][0]] = pairs[i][1]
         }
         return res 
     }
@@ -150,14 +147,8 @@ var zhanglezl = function () {
     }
 
     function dropRight(ary, n = 1) {
-        var res = []
-        if (ary.length <= n) {
-            return []
-        }
-        for (var i = ary.length - 1 - n ; i >= 0 ; i--) {
-            res.push(ary[i])
-        }
-        return res
+        if (n == 0) return ary;
+        return ary.slice(0, -n)
     }
 
     function reverse(ary) {
@@ -179,6 +170,7 @@ var zhanglezl = function () {
         return i + 1
     }
     function every(ary, test) {
+        test = process(test)
         for (var i = 0; i < ary.length; i++) {
             if (!test(ary[i])) return false
         }
@@ -186,6 +178,7 @@ var zhanglezl = function () {
     }
 
     function filter(ary, test) {
+        test = process(test)
         var res = []
         for (var i = 0; i < ary.length; i++) {
             if (test(ary[i])) {
@@ -196,17 +189,19 @@ var zhanglezl = function () {
     }
     
     function find(ary, test) {
+        test = process(test)
         for (var i = 0; i < ary.length; i++) {
             if (test(ary[i])) return ary[i]
         }
     }
 
     function toArray(val) {
+        var res = []
         if (typeof val == "string") {
             return val.split("")
         }
         if (typeof val == "object") {
-            var res = []
+           
             for (key in val) {
                 res.push(val[key])
             }
@@ -216,6 +211,7 @@ var zhanglezl = function () {
     }
 
     function max(ary) {
+        if(!ary.length) return undefined;
         var max = -Infinity
         for (var i = 0; i < ary.length; i++) {
             if (max < ary[i]) {
@@ -225,15 +221,17 @@ var zhanglezl = function () {
         return max 
     }
     function maxBy(ary,test) {
-        var max = 0
+        var max = -Infinity
+        test = process(test)
         for (var i = 0; i < ary.length; i++) {
-            if (test(ary[max]) < test(ary[i])) {
+            if (max < test(ary[i])) {
                 max = i
             }
         }
         return ary[max]  
     }
     function min(ary) {
+        if(ary.length == 0) return undefined;
         var min = Infinity
         for (var i = 0; i < ary.length; i++) {
             if (min > ary[i]) {
@@ -243,9 +241,10 @@ var zhanglezl = function () {
         return min 
     }
     function minBy(ary,test) {
-        var min = 0
+        var min = Infinity
+        test = process(test)
         for (var i = 0; i < ary.length; i++) {
-            if (test(ary[min]) > test(ary[i])) {
+            if (min > test(ary[i])) {
                 min = i
             }
         }
@@ -256,7 +255,8 @@ var zhanglezl = function () {
             return a + b
         },0)
     }
-    function sumBy(ary,test = it => it) {
+    function sumBy(ary,test) {
+        test = process(test)
         return ary.reduce(function(a,b) { 
             return test(a) + test(b)
         }) - test(ary[0])
@@ -264,15 +264,26 @@ var zhanglezl = function () {
     function curry(fn) {
         return function curried(...args) {
             if (args.length < fn.length) {
-                return function (...args2) {
-                    return curriedfn (...args,args2)
+                return function(...args2) {
+                    return curried(...args,...args2)
                 }
 
             }
             return fn(...args)
         }
-
     }
+
+
+    function curry2(f, length = f.length) {
+        return function(...args) {
+            if (args.length < length) {
+                return curry(f.bind(null, args), length - args.length)
+            } else {
+                return f(...args) 
+            }
+        }
+    }
+    
     function concat(initArray, ...values) {
         var res = initArray 
         for (var i = 0; i < values.length; i++) {
@@ -287,10 +298,11 @@ var zhanglezl = function () {
         return res
     }
     
-    function groupBy(array, predicate = it => it) {   // if predicate is not inputed , ju
-        var res = {}                                  // just output itself
+    function groupBy(array, predicate) {   // if predicate is not inputed , ju
+        var res = {}       
+        predicate = process(predicate)                           // just output itself
         for (var i = 0; i < array.length; i++) {
-            var key = predicate(arrary[i], i, array) 
+            var key = predicate(array[i]) 
             if (!Array.isArray(res[key])) {
                 res.key = []
             } else {
@@ -301,6 +313,7 @@ var zhanglezl = function () {
     
     function mapValues(obj, mapper) {
         var res = {}
+        mapper = process(mapper)
         for (var key in obj) {
             var val = obj[key]
             res[key] = mapper(val, key, obj)
@@ -317,8 +330,9 @@ var zhanglezl = function () {
     } 
     function map(ary, mapper) {
         var res = [] 
+        mapper = process(mapper)
         for (var i = 0; i < ary.length; i++) {
-            res.push (mapper(ary[i]), i ,ary)
+            res.push(mapper(ary[i]), i ,ary)
         }
         return res  
     }
@@ -337,8 +351,13 @@ var zhanglezl = function () {
         var res = [].concat(...values)
         return ary.filter(it => res.indexOf(it) == -1)
     }
+    function differenceBy(ary, ...values, iteratee = it => it) {
+        var res = [].concat(...values)
+        iteratee = process(iteratee)
+        return ary.filter(it => res.every(re => iteratee(re) !== iteratee(it)))
+    }
 
-    function intersection([values]) { 
+    function intersection(...values) { 
         var res = []
         for (var i = 0; i < values[0].length; i++) {
             for (var j = 1; j < values.length; j++) {
@@ -352,8 +371,123 @@ var zhanglezl = function () {
             return res
         }
     }
+
+    function property(path) {
+        var names = path.split(".")
+        return function(obj) {
+            for (var name of names) {
+                if (name in Object(obj)){
+                    obj = obj[name]    
+                } else {
+                    return
+                }
+            }
+            return obj
+        }
+    }
+
+    function get(object, path, defaultValue) {
+        var names = path.split(".")
+            for (var name of names) {
+                if (name in Object(object)){
+                    object = object[name]    
+                } else {
+                    return defaultValue
+                }
+            }
+        return object
+    }
+
+    function isMatch(src, obj) {
+        for (var key in src) {
+            if (src[key] && typeof sce[key] == 'object') {
+                if (!isMatch(src[key], obj[key])) {
+                    return false 
+                }
+            } else {
+                if (obj[keyh] !== src[key]) {
+                    return false 
+                }
+            }
+        } 
+        return true 
+    } 
+
+    function matches(src) {
+        return _.bind(isMatch,null, _, src)
+    } 
+    function bind(f, thisArg, ...partials) {
+        return function (...args) {
+            var copy = partials.slice()
+            for (var i = 0; i < copy.length; i++ ) {
+                if (copy[i] == window) {
+                    copy[i] = args.shift()
+                }
+            }
+            return f.call(thisArg, ...copy, ...args)
+        }
+    }
+
+    function ary(f, n = f.length) {
+         return function(...args) {
+            
+        }
+    }
+    function before(n, func) {
+        var c = 0
+        var res 
+        return function (...args) {            
+            if (c < n) {
+                return res = func.call(this, ...args)
+                c++
+            } else {
+                return res 
+            }
+        }
+    }
+
+    function after(n, func) {
+        var c = 0
+        return function(...args) {
+            c ++
+            if (c > n) {
+                return func.call(this, ...args) 
+            }
+            
+        }
+    }
+
+    function flip(func) {
+        return function(...args) {
+            return func(...args.reverse())
+        }
+    }
+
+    function negate(predicate) {
+        return function(...args) {
+            return !predicate(...args)
+        }
+    }
+
+    function spread(func,start = 0) {         // func 绑定function的this 
+        return function(ary) {               //  func.call(this, ...ary)
+            return func.apply(this, ary)    // func.apply.bind(func, this)(ary)
+        }                              
+    }
+
+    function process(predicate) {
+        if (isFunction(predicate)) {
+          return predicate
+        } else if (isObject(predicate)) {
+          return matches(predicate)
+        } else if (isArray(predicate)) {
+          return matchesProperty(predicate)
+        } else if (isString(predicate)) {
+          return property(predicate)
+        }
+      }
     return {
-        compact, chunk, join, last, lastindexof, fill, findIndex, findLastIndex, flatten, flattenDeep, flattenDepth, fromPairs, head, indexOf, initial , drop, dropRight, reverse, sortedIndex, max, maxBy, min, minBy, sum, sumBy, find, toArray, every, filter, curry, concat, groupBy, mapValues, map, some, identity, mapKeys, difference, intersection, 
+        compact, chunk, join, last, lastindexof, fill, findIndex, findLastIndex, flatten, flattenDeep, flattenDepth, fromPairs, head, indexOf, initial , drop, dropRight, reverse, sortedIndex, max, maxBy, min, minBy, sum, sumBy, find, toArray, every, filter, curry, concat, groupBy, mapValues, map, some, identity, mapKeys, difference, intersection, property, get, isMatch, matches, bind, 
     }
 
 }()
